8f950afcd5c31133b61632610d44f3c439d46503
Mattn4 <nky_88@hotmail.com>
2023-11-25 22:49:25 +0800

    Minor editing

--- ToDoList/src/main/java/com/example/ToDoList/controllers/TaskController.java
+++ ToDoList/src/main/java/com/example/ToDoList/controllers/TaskController.java
@@ -35,27 +35,11 @@ public class TaskController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-//    public ResponseEntity<Object> getAllTasks() {
-//
-//        List<Task> result = taskService.getAllTask();
-//
-//        if(!result.isEmpty()){
-//            return new ResponseEntity<>(result, HttpStatus.OK);
-//        }else{
-//            return new ResponseEntity<>("No task(s) found", HttpStatus.NOT_FOUND);
-//        }
-//    }
-
-//    public ResponseEntity<List<Task>> getAllTasks() {
-//        return ResponseEntity.ok(taskService.getAllTask());
-//    }
-
     @GetMapping("/{id}")
     public ResponseEntity<Object> getSingleTask(@PathVariable Long id){
 
         Task result = taskService.getTask(id).orElseThrow(() -> new TaskNotFoundException(id));
         return new ResponseEntity<>(result, HttpStatus.OK);
-
     }
 
     @GetMapping("/completed")
@@ -80,32 +64,12 @@ public class TaskController {
         }
 
         return new ResponseEntity<>(result, HttpStatus.OK);
-
     }
 
     @PostMapping()
     public ResponseEntity<Object> createTask(@Valid @RequestBody Task task) {
         return new ResponseEntity<>(taskService.createNewTask(task), HttpStatus.CREATED);
     }
-
-//    @PostMapping()
-//    public ResponseEntity<Task> createTask(@Valid @RequestBody Task task) {
-//        return ResponseEntity.ok(taskService.createNewTask(task));
-//    }
-
-//    @PostMapping()
-//    public ResponseEntity<Object> createTask(@Valid @RequestBody Task task) {
-//        taskService.createNewTask(task);
-//        return new ResponseEntity<>("Task is created successfully!!", HttpStatus.CREATED);
-//    }
-
-
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
-//        task.setId(id);
-//        return ResponseEntity.ok(taskService.updateTask(task));
-//        //return new ResponseEntity<>(taskService.updateTask(task), HttpStatus.OK);
-//    }
 
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateTask(@PathVariable Long id, @Valid @RequestBody Task task) {
@@ -114,9 +78,7 @@ public class TaskController {
         Optional<Task> result = taskService.updateTask(id, task);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
-
     }
-
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteTask(@PathVariable Long id) {
@@ -126,13 +88,5 @@ public class TaskController {
 
         return new ResponseEntity<>("Task is deleted successfully.", HttpStatus.OK);
     }
-
-//    @DeleteMapping("{id}")
-//    public ResponseEntity<String> deleteTasks(@PathVariable("id") Long id){
-//        String deletedtask = taskService.findTaskById(id).getTask();
-//        taskService.deleteTask(id);
-//        return ResponseEntity.ok("Task of " + deletedtask + "  is deleted successfully!.");
-//    }
-
 
 }
---
+++ ToDoList/src/main/java/com/example/ToDoList/services/TaskServiceImpl.java
@@ -0,0 +1,69 @@
+package com.example.ToDoList.services;
+
+import com.example.ToDoList.models.Task;
+import com.example.ToDoList.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskServiceImpl implements TaskService{
+
+    private final TaskRepository taskRepository;
+
+    public TaskServiceImpl(@Autowired TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+//    public void createNewTask(Task task) {
+//        taskRepository.save(task);
+//    }
+
+    @Override
+    public Task createNewTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public List<Task> getAllTask() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Optional<Task> getTask(Long id){
+        return taskRepository.findById(id);
+    }
+
+    @Override
+    public List<Task> findAllCompletedTask() {
+        return taskRepository.findByCompletedTrue();
+    }
+
+    @Override
+    public List<Task> findAllInCompleteTask() {
+        return taskRepository.findByCompletedFalse();
+    }
+
+    @Override
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Task> updateTask(Long id, Task task) {
+
+        Optional<Task> result = taskRepository.findById(id);
+
+        try {
+            Task temp = result.get();
+            temp.setTask(task.getTask());
+            temp.setCompleted(task.getCompleted());
+            return Optional.of(taskRepository.save(temp));
+        } catch (Exception e) {
+            return result;
+        }
+    }
+
+}
